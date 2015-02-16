dc4bb1b2eda95f4be8cccd5d5410e243ae6b1b3f
yafeilee <lyfi2003@gmail.com>
2015-02-16 21:16:31 +0800

    lina install file

--- lib/generators/templates/lina.rb
+++ lib/generators/templates/lina.rb
@@ -3,16 +3,16 @@ Lina.setup do |config|
   #是否自动生成 views
   #config.generate_views = true
 
-  #是否开启参数校验
+  #是否开启入口参数校验
   #config.params_check = true
 
-  #校验时使用的 json schema 版本, 更多: http://tools.ietf.org/html/draft-zyp-json-schema-04
+  #入口参数校验时使用的 json schema 版本, 更多: http://tools.ietf.org/html/draft-zyp-json-schema-04
   #config.params_json_schema_version = :v4
 
 
-  #是否开启参数校验
+  #是否开启返回值参数校验
   #config.return_check = true
 
-  #校验时使用的 json schema 版本, 更多: http://tools.ietf.org/html/draft-zyp-json-schema-04
+  #返回值校验时使用的 json schema 版本, 更多: http://tools.ietf.org/html/draft-zyp-json-schema-04
   #config.return_json_schema_version = :v4
 end
