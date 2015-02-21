75c0b3b1cae4366d884bceedf239deafb2e4af33
yafeilee <lyfi2003@gmail.com>
2015-02-21 20:54:50 +0800

    add api params&return schema test

--- test/controllers/lina/schema_controller_test.rb
+++ test/controllers/lina/schema_controller_test.rb
@@ -17,5 +17,34 @@ module Lina
       assert_raise(Lina::ApiSpecError, &a)
     end
 
+    test "Lina::ApiSpecParamsError" do
+      a = -> {
+        class WrongAPiSpecController < Lina::ApplicationController
+          define_action :index, {
+            name: 'index',
+            params: { type: 'object1' },
+            return: {}
+          } do
+          end
+        end
+      }
+
+      assert_raise(Lina::ApiSpecParamsError, &a)
+    end
+
+    test "Lina::ApiSpecReturnError" do
+      a = -> {
+        class WrongAPiSpecController < Lina::ApplicationController
+          define_action :index, {
+            name: 'index',
+            params: {},
+            return: { type: 'object1' }
+          } do
+          end
+        end
+      }
+
+      assert_raise(Lina::ApiSpecReturnError, &a)
+    end
   end
 end
