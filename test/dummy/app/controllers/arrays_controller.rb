98450459b462dd014b069c77628dc54fa063e0a8
yafeilee <lyfi2003@gmail.com>
2015-02-26 17:13:54 +0800

    deal with apidoc type info: array, oneOf, boolean, etc.

--- test/dummy/app/controllers/arrays_controller.rb
+++ test/dummy/app/controllers/arrays_controller.rb
@@ -39,4 +39,95 @@ class ArraysController < Lina::ApplicationController
       render json: [ { wrong: 'wrong item' } ]
     end
   end
+
+  define_action :string, {
+    name: 'string return test',
+    params: {},
+    return: {
+      type: 'string',
+      description: "return value\nstring",
+      minLength: 5,
+    }
+  } do
+    render json: 'hello world'
+  end
+
+  define_action :boolean, {
+    name: 'boolean return test',
+    params: {},
+    return: {
+      type: 'boolean',
+      description: <<-EOF
+return value
+boolean test
+EOF
+    }
+  } do
+    render json: true
+  end
+
+  define_action :null, {
+    name: 'null return test',
+    params: {},
+    return: {
+      type: 'null',
+    }
+  } do
+    render json: null
+  end
+
+  define_action :array_object, {
+    name: 'array object return test',
+    params: {},
+    return: {
+      type: 'array',
+      items: {
+        type: 'object',
+        description: 'array item object description',
+        required: [ :name ],
+        properties: {
+          name: {
+            type: 'string',
+            description: 'array item name description',
+            minLength: 5,
+          }
+        }
+      }
+    }
+  } do
+    render json: [ { name: 'hello world' } ]
+  end
+
+  define_action :array_object_nested_array, {
+    name: 'array object nested array return test',
+    params: {},
+    return: {
+      type: 'array',
+      items: {
+        type: 'object',
+        description: 'array item object description',
+        required: [ :name ],
+        properties: {
+          name: {
+            type: 'array',
+            description: 'array item name description',
+            items: {
+              type: 'string',
+              minLength: 3,
+            }
+          },
+          content: {
+            type: 'string',
+            description: 'array item string description',
+          },
+          zcontent2: {
+            type: 'string',
+            description: 'array item string description',
+          }
+        }
+      }
+    }
+  } do
+    render json: [ { name: [ 'name' ]  }, { name: [ 'name' ] } ]
+  end
 end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
 
-  get 'arrays/index'
-
   namespace :admin do
     get 'posts/index'
   end
 
   mount Lina::Engine => "/lina"
+
+  resources :arrays, only: [:index] do
+    collection do
+      get :string
+      get :boolean
+      get :null
+      get :array_object
+      get :array_object_nested_array
+    end
+  end
 
   resources :posts, only: [:index, :show, :update] do
     resources :labels, only: [:index]
